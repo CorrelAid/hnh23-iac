6a476b8c28c75929503aaa4ba32794a71589de25
Jonas Stettner <mail@jstet.net>
2023-06-04 01:14:47 +0200

    test add ubuntu container

--- main.tf
+++ main.tf
@@ -144,3 +144,9 @@ provider "docker" {
 resource "docker_image" "ubuntu" {
   name = "ubuntu:latest"
 }
+
+# Create a container
+resource "docker_container" "foo" {
+  image = docker_image.ubuntu.image_id
+  name  = "foo"
+}
