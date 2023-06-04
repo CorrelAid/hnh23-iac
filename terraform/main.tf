3be7e15db8894f52cfb54f1de68a2bdeb12a236f
Jonas Stettner <mail@jstet.net>
2023-06-04 04:15:27 +0200

    var fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -146,7 +146,7 @@ resource "github_repository" "main" {
 }
 
 resource "github_repository_file" "hosts" {
-  repository = github_repository.foo.name
+  repository = github_repository.main.name
   branch     = "main"
   file       = "./ansible/hosts"
   content = templatefile("inventory.tmpl",
@@ -162,7 +162,7 @@ resource "github_repository_file" "hosts" {
 
 
 resource "github_repository_file" "group_vars" {
-  repository = github_repository.foo.name
+  repository = github_repository.main.name
   branch     = "main"
   file       = "./ansible/group_vars/main.yml"
   content = templatefile("group_vars.tmpl",
