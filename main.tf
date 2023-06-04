f5b2641fee3489cac0c40399cacc9d4647dc6573
Jonas Stettner <mail@jstet.net>
2023-06-04 02:12:44 +0200

    typo depends on

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "docker" {
 module "docker_containers" {
   source = "./docker_containers"
   depends_on = [
-    module.module.docker_host
+    module.docker_host
   ]
   host_ip = module.docker_host.host_ip
   ssh_key = module.docker_host.ssh_key
