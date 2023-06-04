bf8ad0ee7d8544ddf4dd375e5cca5bee84a090c2
Jonas Stettner <mail@jstet.net>
2023-06-04 02:18:23 +0200

    no docker provider for docker host necessary

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "docker_host" {
-  source = "./docker_host"
-  smtp_pw = var.smtp_pw
-  smtp_user = var.smtp_user 
-  directus_admin_pw = var.directus_admin_pw 
+  source              = "./docker_host"
+  smtp_pw             = var.smtp_pw
+  smtp_user           = var.smtp_user
+  directus_admin_pw   = var.directus_admin_pw
   directus_admin_mail = var.directus_admin_mail
-  ssh_key = var.ssh_key
+  ssh_key             = var.ssh_key
 }
 
 provider "docker" {
-  alias  = "main"
+  alias    = "main"
   host     = "ssh://root@${hcloud_server.main.ipv4_address}:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", "<(echo ${tls_private_key.ssh.private_key_pem})"]
 }
@@ -20,7 +20,7 @@ module "docker_containers" {
   ]
   host_ip = module.docker_host.host_ip
   ssh_key = module.docker_host.ssh_key
-    providers = {
+  providers = {
     docker.main_ = docker.main
   }
 }
