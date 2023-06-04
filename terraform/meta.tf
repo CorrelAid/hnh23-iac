79ed6dd449853c4eea41e332304b9b2953262b02
Jonas Stettner <mail@jstet.net>
2023-06-04 14:31:33 +0200

    back to newer vers

--- terraform/meta.tf
+++ terraform/meta.tf
@@ -10,12 +10,12 @@ terraform {
     }
      github = {
       source  = "integrations/github"
-      version = "4.18.0"
+      version = "5.26.0"
     }
     
   }
 }
 
 provider "github" {
-  
+
 }
