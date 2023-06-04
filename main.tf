59894e0bb7e9df0217466243b1f7ff15a3384806
Jonas Stettner <mail@jstet.net>
2023-06-04 02:25:17 +0200

    pass down all provider config

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ module "docker_host" {
 }
 
 provider "docker" {
-  alias    = "main"
   host     = "ssh://root@${hcloud_server.main.ipv4_address}:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", "<(echo ${tls_private_key.ssh.private_key_pem})"]
 }
@@ -20,7 +19,5 @@ module "docker_containers" {
   ]
   host_ip = module.docker_host.host_ip
   ssh_key = module.docker_host.ssh_key
-  providers = {
-    docker.main_ = docker.main
-  }
+ 
 }
